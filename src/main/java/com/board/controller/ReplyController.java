0c1bb8eeb74677d957ef3121fb567d2cfcc02e73
ParkYoungUn <youngun45@naver.com>
2020-12-29 14:43:26 +0900

    댓글 삭제 구현완료 및 코드정리

--- src/main/java/com/board/controller/ReplyController.java
+++ src/main/java/com/board/controller/ReplyController.java
@@ -33,12 +33,8 @@ public class ReplyController {
 	//댓글 삭제
 	@RequestMapping(value = "/delete", method=RequestMethod.GET)
 	public String getDelete(ReplyVO vo) throws Exception {
-		System.out.println("rvo.getBno(): " + vo.getBno());
-		replyService.delete(vo);
 
-//		return "redirect:/board/view?rno=" + vo.getRno();
-//		return "/board/view?bno=" + rvo.getBno();
-		System.out.println("rvo.getBno(): " + vo.getBno());
+		replyService.delete(vo);
 
 		return "redirect:/board/view?bno=" + vo.getBno();
 	}
